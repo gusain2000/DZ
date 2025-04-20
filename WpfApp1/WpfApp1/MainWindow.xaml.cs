603ad925c19a07c3846ed520fbe3b6e694f02419
gusain2000 <gusain208@gmail.com>
2025-04-20 19:48:34 +0400

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileDownloaderApp
+{
+    public partial class MainWindow : Window
+    {
+        private static Mutex _mutex = new Mutex();
+        private static List<string> _fileUrls = new List<string>
+        {
+            "https://images.hdqwalls.com/download/flash-barry-allen-2022-4k-a2-3840x2160.jpg", // Example file URLs
+            "https://images.wallpapersden.com/image/download/marvel-spider-man-hd-art-2022_bWZqbWyUmZqaraWkpJRsa21lrWloZ2U.jpg",
+            "https://i.ytimg.com/vi/u6RRZf5q0t0/maxresdefault.jpg"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void DownloadFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusListBox.Items.Clear();
+            List<Task> downloadTasks = new List<Task>();
+
+            foreach (var url in _fileUrls)
+            {
+                downloadTasks.Add(DownloadFileAsync(url));
+            }
+            await Task.WhenAll(downloadTasks);
+            
+            StatusListBox.Items.Add("All files have been downloaded!");
+        }
+
+        private async Task DownloadFileAsync(string url)
+        {
+            AddStatus($"Thread started downloading: {url}");
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        _mutex.WaitOne();
+                        try
+                        {
+                            AddStatus($"Download completed for {url}.");
+                        }
+                        finally
+                        {
+                            _mutex.ReleaseMutex();
+                        }
+                    }
+                    else
+                    {
+                        AddStatus($"Error downloading from {url}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AddStatus($"Error downloading {url}: {ex.Message}");
+                }
+            }
+            
+            AddStatus($"Thread finished downloading: {url}");
+        }
+
+        private void AddStatus(string message)
+        {
+            Dispatcher.Invoke(() => StatusListBox.Items.Add(message));
+        }
+    }
+}
