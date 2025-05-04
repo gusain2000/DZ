0220ad62648440bec1f27d092afaadc06b818874
gusain2000 <gusain208@gmail.com>
2025-05-04 16:42:25 +0400

    Add files via upload

---
+++ Network/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        var client = new TcpClient("127.0.0.1", 13000);
+        Console.WriteLine("Подключено к серверу.");
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
+
+        while (true)
+        {
+            Console.Write("Вы: ");
+            string message = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Сервер: " + response);
+        }
+        
+    }
+}
---
+++ Network/Server/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 13000);
+        server.Start();
+        Console.WriteLine("Сервер запущен. Ожидание клиента...");
+
+        var client = server.AcceptTcpClient();
+        Console.WriteLine("Клиент подключён.");
+
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Клиент: " + message);
+            
+            Console.Write("Я: ");
+            string response = Console.ReadLine();
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
